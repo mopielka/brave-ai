1fd48351d2e17dd83ee1d40afad3369ebe877667
Michal Opielka <michal.opielka@finder.com>
2023-10-25 10:44:11 +0200

    feat: add automatic solver

--- src/solver.ts
+++ src/solver.ts
@@ -1,19 +1,37 @@
-import {TaskPayload} from "./types";
+import { TaskPayload } from './types';
+import { OpenAI } from 'openai';
+
+const DEFAULT_MODEL = 'gpt-3.5-turbo';
+const GPT_SYSTEM_INSTRUCTIONS = `
+  Act as a system for automated solving tasks specified in JSON format.
+  User sends a JSON with potential error code (if any), task description and other payload data, for example:
+  {"code":0,"msg":"Name 3 similar fruits to the one in this payload","fruit":"orange"}
+  
+  Please respond exclusively with what was requested in the task, for example:
+  lemon, lime, tangerine`;
 
 const solvers: {[key: string]: (taskPayload: TaskPayload) => Promise<string|any[]|undefined>} = {
-  // gpt: (taskPayload: TaskPayload): Promise<string|any[]> => {
-  //
-  // },
+  gpt: async (taskPayload: TaskPayload): Promise<string|any[]|undefined> => {
+    const completion = await new OpenAI().chat.completions.create({
+      messages: [
+        { role: "system", content: GPT_SYSTEM_INSTRUCTIONS },
+        { role: "user", content: JSON.stringify(taskPayload) },
+      ],
+      model: DEFAULT_MODEL,
+    });
+
+    return completion.choices[0].message.content ?? undefined;
+  },
   helloapi: async (taskPayload: TaskPayload): Promise<string|any[]|undefined> => {
     return taskPayload.cookie;
   }
 }
 
 export const generateSolution = async (taskName: string, taskPayload: TaskPayload): Promise<string|(any[])> => {
-  let solution: string | any[] | undefined = await solvers[taskName](taskPayload);
+  let solution: string | any[] | undefined = await solvers[taskName]?.(taskPayload);
 
   if (typeof solution === 'undefined') {
-    solution = undefined; // await solvers.ai[taskPayload];
+    solution = await solvers.gpt(taskPayload);
   }
 
   if (typeof solution === 'undefined') {
